3b60897c69998836da7843e4d35b68ac6535a407
Francerz <francerzam@hotmail.com>
2020-11-28 17:39:45 -0600

    GruposResource para Estudiantes compatible

---
+++ src/Resources/Escolares/GruposResource.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ITColima\Siitec2\Api\Resources\Escolares;
+
+use Francerz\Http\Utils\MessageHelper;
+use ITColima\Siitec2\Api\AbstractResource;
+
+class GruposResource extends AbstractResource
+{
+    public function getAll(array $params = [])
+    {
+        $this->requiresAccessToken(false);
+        $response = $this->get('/escolares/grupos', $params);
+        return MessageHelper::getContent($response);
+    }
+
+    public function getById($grupo_id)
+    {
+        if (is_array($grupo_id)) {
+            $grupo_id = join('+', $grupo_id);
+        }
+        $this->requiresAccessToken(false);
+        $response = $this->get("/escolares/grupos/{$grupo_id}");
+        return MessageHelper::getContent($response);
+    }
+
+    public function getAsDocente(array $params = [])
+    {
+        $this->requiresAccessToken(true);
+        $response = $this->get('/escolares/grupos/@docente', $params);
+        return MessageHelper::getContent($response);
+    }
+
+    public function getAsEstudiante(array $params = [])
+    {
+        $this->requiresAccessToken(true);
+        $response = $this->get('/escolares/grupos/@estudiante', $params);
+        return MessageHelper::getContent($response);
+    } 
+}
--- src/Scopes.php
+++ src/Scopes.php
@@ -4,17 +4,8 @@ namespace ITColima\Siitec2\Api;
 
 interface Scopes
 {
-    const GET_FULL_ACCESS_USER = 'self.all:r';
-    const GET_FULL_ACCESS_APP = 'app.all:r';
-
+    const GET_FULL_ACCESS_USER = 'all.own:r';
     const GET_USUARIO_PERFIL_OWN= 'usuarios.perfil.own:r';
-    const GET_USUARIO_PERFIL_BY_ID = 'usuarios.perfil.by-id:r';
-
-    const GET_DOCENCIA_GRUPOS_ALL = 'docencia.grupos.all:r';
-    const GET_DOCENCIA_GRUPOS_OWN = 'docencia.grupos.own:r';
-    const GET_DOCENCIA_GRUPOS_BY_ID = 'docencia.grupos.by-id:r';
-
-    const GET_ESCOLARES_PERIODOS_ALL = 'escolares.periodos.all:r';
-    const GET_ESCOLARES_ASIGNATURAS_ALL = 'escolares.asignaturas.all:r';
-    const GET_ESCOLARES_PLANES_ESTUDIO_ALL = 'escolares.planes-estudio.all:r';
+    const GET_ESCOLARES_GRUPOS_DOCENTE = 'escolares.grupos.docente:r';
+    const GET_ESCOLARES_GRUPOS_ESTUDIANTE = 'escolares.grupos.estudiante:r';
 }
