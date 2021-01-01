4cfecfea45fe8fd8fe9497e006ea3454c74575f7
Francerz <francerzam@hotmail.com>
2021-01-01 00:31:57 -0600

    Added SIITEC2_URI constant

--- src/Constants.php
+++ src/Constants.php
@@ -4,6 +4,8 @@ namespace ITColima\Siitec2\Api;
 
 class Constants
 {
+    const SIITEC2_URI = 'https://siitec2.colima.tecnm.mx';
+
     const AUTHORIZE_ENDPOINT = 'https://siitec2.colima.tecnm.mx/index.php/oauth2/authorize';
     const TOKEN_ENDPOINT = 'https://siitec2.colima.tecnm.mx/index.php/oauth2/token';
     const API_ENDPOINT = 'https://siitec2.colima.tecnm.mx/api/index.php';
