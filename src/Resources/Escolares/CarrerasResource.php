58a6a53f2d8a110037b795ed508ae68572dc4889
Francerz <francerzam@hotmail.com>
2021-01-26 11:20:25 -0600

    Added Escolares/CarrerasResource

---
+++ src/Resources/Escolares/CarrerasResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ITColima\Siitec2\Api\Resources\Escolares;
+
+use Francerz\Http\Utils\MessageHelper;
+use ITColima\Siitec2\Api\AbstractResource;
+
+class CarrerasResource extends AbstractResource
+{
+    public function getAll(array $params = [])
+    {
+        $this->requiresAccessToken(false);
+        $response = $this->get('/escolares/carreras', $params);
+        return MessageHelper::getContent($response);
+    }
+
+    public function getById($carrera_id, array $params = [])
+    {
+        if (is_array($carrera_id)) {
+            $carrera_id = join('+', $carrera_id);
+        }
+        $this->requiresAccessToken(false);
+        $response = $this->get("/escolares/carreras/{$carrera_id}", $params);
+        return MessageHelper::getContent($response);
+    }
+}
