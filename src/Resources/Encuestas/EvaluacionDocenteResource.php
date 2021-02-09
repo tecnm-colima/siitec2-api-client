9c6727aabf6cd792c4fe701596644879caa76d38
Francerz <francerzam@hotmail.com>
2021-02-08 20:52:27 -0600

    List all encuestas/edocente

--- src/Resources/Encuestas/EvaluacionDocenteResource.php
+++ src/Resources/Encuestas/EvaluacionDocenteResource.php
@@ -7,9 +7,16 @@ use ITColima\Siitec2\Api\AbstractResource;
 
 class EvaluacionDocenteResource extends AbstractResource
 {
+    public function getAll(array $params = [])
+    {
+        $this->requiresClientAccessToken(true);
+        $response = $this->get('/encuestas/edocente', $params);
+        return MessageHelper::getContent($response);
+    }
+
     public function getRealizada($encuesta_id, $usuario_id, array $params = [])
     {
-        $this->requiresClientAccessToken(false);
+        $this->requiresClientAccessToken(true);
         $response = $this->get("/encuestas/edocente/{$encuesta_id}/usuarios/{$usuario_id}", $params);
         return MessageHelper::getContent($response);
     }
