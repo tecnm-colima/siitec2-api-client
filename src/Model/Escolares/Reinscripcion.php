1fdbd728bb6a40eb5cbd4760cbcb9317959dbd9c
Francerz <francerzam@hotmail.com>
2021-02-10 22:16:23 -0600

    Added __construct() to Reinscripcion

--- src/Model/Escolares/Reinscripcion.php
+++ src/Model/Escolares/Reinscripcion.php
@@ -6,4 +6,10 @@ class Reinscripcion
 {
     public $id_estudiante;
     public $id_periodo;
+
+    public function __construct($id_estudiante = null, $id_periodo = null)
+    {
+        $this->id_estudiante = $id_estudiante;
+        $this->id_periodo = $id_periodo;
+    }
 }
