aff371c61e43f322074c2b77346b1a68060122fa
Francerz <francerzam@hotmail.com>
2021-06-22 13:09:52 -0500

    ADD Kardex->aprobatoria

--- src/Model/Escolares/Kardex.php
+++ src/Model/Escolares/Kardex.php
@@ -9,18 +9,21 @@ class Kardex
     public $periodo_id;
     public $oportunidad;
     public $calificacion;
+    public $aprobatoria;
 
     public function __construct(
         $alumno_id,
         $materia_id,
         $periodo_id,
         $calificacion = '0.00',
-        $oportunidad = 1
+        $oportunidad = 1,
+        $aprobatoria = 0
     ) {
         $this->alumno_id = $alumno_id;
         $this->materia_id = $materia_id;
         $this->periodo_id = $periodo_id;
         $this->calificacion = $calificacion;
         $this->oportunidad = $oportunidad;
+        $this->aprobatoria = $aprobatoria;
     }
 }
