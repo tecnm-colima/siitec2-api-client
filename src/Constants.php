835bfe2aff21bcb0cd7a22b2190872752f9c51eb
Francerz <francerzam@hotmail.com>
2021-07-12 11:17:04 -0500

    CHG renamed domain to siitec.colima.tecnm.mx

--- src/Constants.php
+++ src/Constants.php
@@ -4,9 +4,9 @@ namespace ITColima\Siitec2\Api;
 
 class Constants
 {
-    const PLATFORM_URL = 'https://siitec2.colima.tecnm.mx';
+    const PLATFORM_URL = 'https://siitec.colima.tecnm.mx';
 
-    const AUTHORIZE_ENDPOINT = 'https://siitec2.colima.tecnm.mx/index.php/oauth2/authorize';
-    const TOKEN_ENDPOINT = 'https://siitec2.colima.tecnm.mx/index.php/oauth2/token';
-    const API_ENDPOINT = 'https://siitec2.colima.tecnm.mx/api/index.php';
+    const AUTHORIZE_ENDPOINT = 'https://siitec.colima.tecnm.mx/index.php/oauth2/authorize';
+    const TOKEN_ENDPOINT = 'https://siitec.colima.tecnm.mx/index.php/oauth2/token';
+    const API_ENDPOINT = 'https://siitec.colima.tecnm.mx/api/index.php';
 }
